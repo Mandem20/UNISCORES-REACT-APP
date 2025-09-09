38dfc806525fa03ba65a9458f7f33789b6f3c0b6
Kojoel <nanaabruquah1s@gmail.com>
2025-09-09 14:07:54 +0000

    feat: create and style the first and second sections of the homepage

--- FRONTEND/UniScores/src/App.tsx
+++ FRONTEND/UniScores/src/App.tsx
@@ -1,11 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.scss'
+import Layout from './Layout/Layout';
+import Home from './pages/Home';
 import Header from './components/Header';
+
 
 function App() {
 
   return (
     <>
-      <Header />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          
+        </Route>
+      </Routes>
     </>
   )
 }
--- FRONTEND/UniScores/src/components/Header.tsx
+++ FRONTEND/UniScores/src/components/Header.tsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom"
+import '../styles/Header.scss'
 
 const Header = () => {
   return (
-    <div>
-        
+    <div className="header-container">
+        <div className="logo">UNISCORES</div>
+        <div className="links">
+          <Link to="">Favourites</Link>
+          <Link to="">Livestream</Link>
+          <Link to="">Highlights</Link>
+          <Link to="">News</Link>
+          <Link to="">Login</Link>
+          <Link to="">Register</Link>
+          <Link to="">Get the app</Link>
+        </div>
     </div>
   )
 }
