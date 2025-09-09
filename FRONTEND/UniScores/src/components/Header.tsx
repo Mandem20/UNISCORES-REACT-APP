7e9a250315adb2b3a1331ecf25e20d2dc4183b71
Kojoel <nanaabruquah1s@gmail.com>
2025-09-09 09:52:06 +0000

    chore: install react router for routing

--- FRONTEND/UniScores/src/components/Header.tsx
+++ FRONTEND/UniScores/src/components/Header.tsx
@@ -1,8 +1,42 @@
+import { Link } from "react-router-dom"
+import '../styles/Header.scss';
 
 const Header = () => {
   return (
-    <div>
-        
+    <div className="header-container">
+        <span className="logo">Uniscore</span>
+        <div className="links">
+            <Link to="">
+              <div className="link-wrapper">
+                <span>Favourite</span>
+              </div>
+            </Link>
+            <Link to="">
+              <div className="link-wrapper">
+                <span>Livestream</span>
+              </div>
+            </Link>
+            <Link to="">
+              <div className="link-wrapper">
+                <span>Highlight</span>
+              </div>
+            </Link>
+            <Link to="">
+              <div className="link-wrapper">
+                <span>News</span>
+              </div>
+            </Link>
+            <Link to="">
+              <div className="link-wrapper">
+                <span>Register</span>
+              </div>
+            </Link>
+            <Link to="">
+              <div className="link-wrapper">
+                <span>Get the app</span>
+              </div>
+            </Link>
+        </div>
     </div>
   )
 }
