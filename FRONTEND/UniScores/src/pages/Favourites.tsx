6b699dbcff82196452481b9077bd1230f6c2af81
Kojoel <nanaabruquah1s@gmail.com>
2025-10-01 08:26:47 -0700

    feat: view favourited matches

--- FRONTEND/UniScores/src/pages/Favourites.tsx
+++ FRONTEND/UniScores/src/pages/Favourites.tsx
@@ -1,10 +1,43 @@
 import MatchFixtures from "../components/MatchFixtures"
+import '../styles/Favourites.scss'
 
 const Favourites = () => {
   return (
-    <div>
-      <MatchFixtures />
-      something here
+    <div className="favourites-page-container">
+      <div className="favourites-page-wrapper">
+        <div className="league-item">
+          <div className="league-header">
+            <span className="league-logo">@</span>
+            <div className="league-info">
+                <div className="league-title">Champions League</div>
+                <div className="league-group">Group A</div>
+            </div>
+            <span className="right-arrow">&rarr;</span>
+          </div>
+          <div className="favourited-matches">
+            <MatchFixtures />
+            <MatchFixtures />
+            <MatchFixtures />
+            <MatchFixtures />
+          </div>
+        </div>
+        <div className="league-item">
+          <div className="league-header">
+            <span className="league-logo">@</span>
+            <div className="league-info">
+                <div className="league-title">Champions League</div>
+                <div className="league-group">Group A</div>
+            </div>
+            <span className="right-arrow">&rarr;</span>
+          </div>
+          <div className="favourited-matches">
+            <MatchFixtures />
+            <MatchFixtures />
+            <MatchFixtures />
+            <MatchFixtures />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
